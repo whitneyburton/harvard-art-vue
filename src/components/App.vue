6524012a42264008b41b804cb6abb85d511ebee5
Whitney Burton <whitneyburton9@gmail.com>
2019-04-16 11:15:54 -0600

    Add dynamic fetchArt method for five cultures

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <h1 v-once>Harvard Art Museums</h1>
-    <h3 v-once>Welcome! Here you can find the art produced by different cultures around the world showcased by Harvard Art Museums.</h3>
-    <button v-on:click="this.currentCulture = 'Uruguayan'">Uruguayan</button>
-    <button v-on:click="this.currentCulture = 'Turkmen'">Turkmen</button>
-    <button v-on:click="this.currentCulture = 'Swedish'">Swedish</button>
-    <button v-on:click="this.currentCulture = 'Bulgarian'">Bulgarian</button>
-    <button v-on:click="this.currentCulture = 'Bohemian'">Bohemian</button>
-    <ul class="art-list">
+    <h3
+      v-once
+    >Welcome! Here you can find the art produced by different cultures around the world showcased by Harvard Art Museums.</h3>
+    <button v-on:click="fetchArt()">Uruguayan</button>
+    <button v-on:click="fetchArt()">Turkmen</button>
+    <button v-on:click="fetchArt()">Swedish</button>
+    <button v-on:click="fetchArt()">Bulgarian</button>
+    <button v-on:click="fetchArt()">Bohemian</button>
+    <hr></hr>
+    <h4 v-if="currentCulture">Current Culture: {{ currentCulture }}</h4>
+    <ul v-if="currentCulture" class="art-list">
       <li class="art-item" v-for="art in artwork" v-bind:key="art.id">
         <img v-bind:src="`${art.primaryimageurl}`" class="artPiece">
       </li>
     </ul>
+    <h4 v-if="!currentCulture">Choose a culture above to get started!</h4>
   </div>
 </template>
 
@@ -23,30 +28,33 @@ export default {
   components: {},
   data() {
     return {
-      artwork: [],
       error: "",
+      artwork: [],
       cultureIDs: {
-        'Uruguayan': 37528893,
-        'Turkmen': 37528812,
-        'Swedish': 37528713,
-        'Bulgarian': 37527048,
-        'Bohemian': 37527021,
-        'NewZealand': 37528182
+        Uruguayan: 37528893,
+        Turkmen: 37528812,
+        Swedish: 37528713,
+        Bulgarian: 37527048,
+        Bohemian: 37527021
       },
-      currentCulture: 'NewZealand'
+      currentCulture: ""
     };
   },
-  mounted() {
-    const url = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&culture=${
-      this.cultureIDs[this.currentCulture]
-    }`;
-    fetch(url)
-      .then(response => response.json())
-      .then(result =>
-        result.records.filter(culture => culture.primaryimageurl !== null)
-      )
-      .then(records => (this.artwork = records))
-      .catch(error => (this.error = error.message));
+  methods: {
+    fetchArt() {
+      const clickedCulture = event.target.innerText;
+      const url = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&culture=${
+        this.cultureIDs[clickedCulture]
+      }`;
+      fetch(url)
+        .then(response => response.json())
+        .then(result =>
+          result.records.filter(culture => culture.primaryimageurl !== null || undefined)
+        )
+        .then(records => (this.artwork = records))
+        .then(records => (this.currentCulture = clickedCulture))
+        .catch(error => (this.error = error.message));
+    }
   }
 };
 </script>
