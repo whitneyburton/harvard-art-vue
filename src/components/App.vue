acd540107947ad37b7022f4086ab16226cb70c90
Whitney Burton <whitneyburton9@gmail.com>
2019-04-16 10:30:10 -0600

    Begin dynamic culture functionality

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
     <h1 v-once>Harvard Art Museums</h1>
-    <h3>Welcome! Here you can find the artists, sitters, printers, donors, authors, and publishers of Harvard Art Museums.</h3>
+    <h3 v-once>Welcome! Here you can find the art produced by different cultures around the world showcased by Harvard Art Museums.</h3>
+    <button v-on:click="this.currentCulture = 'Uruguayan'">Uruguayan</button>
+    <button v-on:click="this.currentCulture = 'Turkmen'">Turkmen</button>
+    <button v-on:click="this.currentCulture = 'Swedish'">Swedish</button>
+    <button v-on:click="this.currentCulture = 'Bulgarian'">Bulgarian</button>
+    <button v-on:click="this.currentCulture = 'Bohemian'">Bohemian</button>
+    <ul class="art-list">
+      <li class="art-item" v-for="art in artwork" v-bind:key="art.id">
+        <img v-bind:src="`${art.primaryimageurl}`" class="artPiece">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,15 +23,29 @@ export default {
   components: {},
   data() {
     return {
-      art: [],
-      error: ""
+      artwork: [],
+      error: "",
+      cultureIDs: {
+        'Uruguayan': 37528893,
+        'Turkmen': 37528812,
+        'Swedish': 37528713,
+        'Bulgarian': 37527048,
+        'Bohemian': 37527021,
+        'NewZealand': 37528182
+      },
+      currentCulture: 'NewZealand'
     };
   },
   mounted() {
-    const url = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&culture=37527759`;
+    const url = `https://api.harvardartmuseums.org/object?apikey=${apiKey}&culture=${
+      this.cultureIDs[this.currentCulture]
+    }`;
     fetch(url)
       .then(response => response.json())
-      .then(result => (this.art = [...this.art, result.records]))
+      .then(result =>
+        result.records.filter(culture => culture.primaryimageurl !== null)
+      )
+      .then(records => (this.artwork = records))
       .catch(error => (this.error = error.message));
   }
 };
@@ -34,6 +58,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+ul {
+  height: 75vh;
+  overflow: scroll;
+}
+
+li {
+  list-style-type: none;
+}
+
+img {
+  width: 80%;
 }
 </style>
